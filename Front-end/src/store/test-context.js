941429cded61f2fe4dea52d34b7ec623be0f3459
zacktaylor12359 <46452540+zacktaylor12359@users.noreply.github.com>
2022-01-06 20:46:29 -0500

    Added Context

--- Front-end/src/store/test-context.js
+++ Front-end/src/store/test-context.js
@@ -1,5 +1,18 @@
 import { createContext } from 'react';
 
-const TestContext = createContext({});
+const TestContext = createContext({
+	header: '',
+	headerAlignment: '',
+	title: '',
+	instructions: '',
+	section: [
+		{
+			section_type: '',
+			section_title: '',
+			section_instructions: '',
+			section_structure: {},
+		},
+	],
+});
 
 export default TestContext;
