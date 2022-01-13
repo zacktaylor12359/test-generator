2f38405bb7f5781e45d9fb2e306fa4f654729105
Zachary Taylor <zataylor@wayne.edu>
2022-01-13 10:39:26 -0500

    section modal updates

--- Front-end/src/Components/UI/SectionModal.js
+++ Front-end/src/Components/UI/SectionModal.js
@@ -1,15 +1,51 @@
-import { useState } from 'react';
+import { useState, Fragment } from 'react';
 import Modal from './Modal';
 import Button from './Button';
 import styles from './SectionModal.module.css';
 
 const SectionModal = (props) => {
-	const [typeSettings, setTypeSettings] = useState('Mulitple Choice');
+	const [sectionTitle, setSectionTitle] = useState(false);
+	const [sectionInstructions, setSectionInstructions] = useState(false);
+	const [numQuestions, setNumQuestions] = useState(1);
+	const [numOptions, setNumOptions] = useState(4);
+	const [typeSettings, setTypeSettings] = useState('MC');
+
+	const sectionTitleChangeHandler = (event) => {
+		event.target.checked === true
+			? setSectionTitle(true)
+			: setSectionTitle(false);
+	};
+
+	const sectionInstructionsChangeHandler = (event) => {
+		event.target.checked === true
+			? setSectionInstructions(true)
+			: setSectionInstructions(false);
+	};
+
 	const typeChangeHandler = (event) => {
-		event.preventDefault();
-		if (event.target.value == 'Multiple Choice') {
+		const option = parseInt(event.target.value);
+		switch (option) {
+			case 1:
+				setTypeSettings('MC');
+				break;
+			default:
+				setTypeSettings('MC');
+				break;
 		}
 	};
+
+	const numQuestionsChangeHandler = (event) => {
+		event.target.value === ''
+			? setNumQuestions(0)
+			: setNumQuestions(parseInt(event.target.value));
+	};
+
+	const numOptionsChangeHandler = (event) => {
+		event.target.value === ''
+			? setNumOptions(0)
+			: setNumOptions(parseInt(event.target.value));
+	};
+
 	return (
 		<Modal>
 			<header className={styles.header}>
@@ -17,36 +53,85 @@ const SectionModal = (props) => {
 			</header>
 			<form className={styles.content}>
 				<div className={styles['form-input']}>
-					<label htmlFor='title'>Section Title:</label>
+					<label htmlFor="title">Section Title:</label>
 
 					<input
-						type='checkbox'
-						id='title'
-						name='title'
-						value='Title'
+						onChange={sectionTitleChangeHandler}
+						type="checkbox"
+						id="title"
+						name="title"
+						value={sectionTitle}
 					/>
 				</div>
 
 				<div className={styles['form-input']}>
-					<label htmlFor='instructions'>Section Instructions:</label>
+					<label htmlFor="instructions">Section Instructions:</label>
 					<input
-						type='checkbox'
-						id='instructions'
-						name='instructions'
-						value='Instructions'
+						onChange={sectionInstructionsChangeHandler}
+						type="checkbox"
+						id="instructions"
+						name="instructions"
+						value={sectionInstructions}
 					/>
 				</div>
 
 				<div className={styles['form-input']}>
-					<label htmlFor='question-type'>Queston Type:</label>
+					<label htmlFor="question-type">Queston Type:</label>
 					<select onChange={typeChangeHandler}>
-						<option value='Multiple Choice'>Multiple Choice</option>
+						<option value="1">Multiple Choice</option>
+						<option value="1">Multiple Choice</option>
 					</select>
 				</div>
+
+				{typeSettings === 'MC' && (
+					<Fragment>
+						<div className={styles['form-input']}>
+							<label htmlFor="answer-options">
+								Number of Questions:
+							</label>
+							<input
+								className={styles['number-input']}
+								onChange={numQuestionsChangeHandler}
+								type="number"
+								id="answer-options"
+								name="answer-options"
+								min="0"
+								max="500"
+								step="1"
+								value={
+									numQuestions > 0
+										? numQuestions.toString()
+										: ''
+								}
+							/>
+						</div>
+
+						<div className={styles['form-input']}>
+							<label htmlFor="answer-options">
+								Number of Answer Options:
+							</label>
+							<input
+								className={styles['number-input']}
+								onChange={numOptionsChangeHandler}
+								type="number"
+								id="answer-options"
+								name="answer-options"
+								min="1"
+								max="10"
+								step="1"
+								value={
+									numOptions > 0 ? numOptions.toString() : ''
+								}
+							/>
+						</div>
+					</Fragment>
+				)}
+
+				<footer className={styles.actions}>
+					<Button onClick={props.onClose}>Cancel</Button>
+					<Button onClick={props.onClose}>Create Test</Button>
+				</footer>
 			</form>
-			<footer className={styles.actions}>
-				<Button onClick={props.onClose}>Okay</Button>
-			</footer>
 		</Modal>
 	);
 };
