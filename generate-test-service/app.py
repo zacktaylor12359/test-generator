c16e658e0b8d012aac13a50923ab04d0614549de
zacktaylor12359 <46452540+zacktaylor12359@users.noreply.github.com>
2022-04-05 12:29:39 -0400

    docker port number fix

--- generate-test-service/app.py
+++ generate-test-service/app.py
@@ -1,20 +1,21 @@
 from flask import Flask
 from flask import jsonify
-import os 
+import os
 app = Flask(__name__)
+
 
 def change(amount):
     # calculate the resultant change and store the result (res)
     res = []
-    coins = [1,5,10,25] # value of pennies, nickels, dimes, quarters
+    coins = [1, 5, 10, 25]  # value of pennies, nickels, dimes, quarters
     coin_lookup = {25: "quarters", 10: "dimes", 5: "nickels", 1: "pennies"}
 
     # divide the amount*100 (the amount in cents) by a coin value
     # record the number of coins that evenly divide and the remainder
     coin = coins.pop()
-    num, rem  = divmod(int(amount*100), coin)
+    num, rem = divmod(int(amount*100), coin)
     # append the coin type and number of coins that had no remainder
-    res.append({num:coin_lookup[coin]})
+    res.append({num: coin_lookup[coin]})
 
     # while there is still some remainder, continue adding coins to the result
     while rem > 0:
@@ -22,7 +23,7 @@ def change(amount):
         num, rem = divmod(rem, coin)
         if num:
             if coin in coin_lookup:
-                res.append({num:coin_lookup[coin]})
+                res.append({num: coin_lookup[coin]})
     return res
 
 
@@ -30,7 +31,8 @@ def change(amount):
 def hello():
     """Return a friendly HTTP greeting."""
     print("I am inside hello world")
-    return 'Hello World! I can makt route: /change'
+    return 'Hello World! I me: /change'
+
 
 @app.route('/change/<dollar>/<cents>')
 def changeroute(dollar, cents):
@@ -39,11 +41,12 @@ def changeroute(dollar, cents):
     result = change(float(amount))
     return jsonify(result)
 
+
 @app.route('/thing/<thing>')
 def testroute(thing):
-    return 'heres the thing ${thing}'
+    return f'heres the thing ${thing}'
 
 
 if __name__ == '__main__':
-    port = os.environ.get("PORT", 5000)
-    app.run(debug=False, host='0.0.0.0', port=port)
+    port = os.environ.get("PORT", 4000)
+    app.run(debug=True, host='0.0.0.0', port=port)
