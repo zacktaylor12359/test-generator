1a15c766562685b0c3dd37d78a2b4d0f1d5831fe
zacktaylor12359 <46452540+zacktaylor12359@users.noreply.github.com>
2022-04-12 18:08:39 -0400

    API generates styled document

--- generate-test-service/app.py
+++ generate-test-service/app.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, send_file, send_from_directory, jsonify
 from pathlib import Path
 from docx import Document
+from docx.shared import Inches, Pt
+from docx.enum.text import WD_ALIGN_PARAGRAPH
 import os
 import zipfile
 import random
 app = Flask(__name__)
-
-document = Document()
 
 
 def change(amount):
@@ -49,33 +49,18 @@ def changeroute(dollar, cents):
 
 @app.route('/thing')
 def testroute():
-    document = Document()
-    document.add_heading('Document Title', 9)
-
-    # data = request.get_json()
-    # questions = data['questionList']
-    testPath = Path.home() / 'Tests'
-    answerPath = Path.home() / 'AnswerKeys'
-
-    if(not(os.path.isdir(testPath))):
-        os.mkdir(testPath)
-
-    # if(not(os.path.isdir(answerPath))):
-    #     os.mkdir(answerPath)
-
-<<<<<<< HEAD
     # testStr = ('Test' + '.doc')
     # answerStr = ('AnswerKey' + '.doc')
     # testFile = open(testPath / testStr, 'w+')
     # answerFile = open(answerPath / answerStr, 'w+')
     # answerFile.write('ANSWER KEY (FORM1)\n')
     # testFile.write('Name:\n\n\nDate:\n\n\nPeriod:\n\n\nState Capitals Quiz (Form1)\n\n\n')
-    # random.shuffle(questions)   
+    # random.shuffle(questions)
 
     # for j in range(50):
     #     rand = random.randint(0, 3)
     #     answerFile.write(str(j + 1) + '. ' + chr(65 + rand) + '\n')
-    #     testFile.write(str(j + 1) + '. What is the capital of ' + questions[j]["state"] + '?\n\n') 
+    #     testFile.write(str(j + 1) + '. What is the capital of ' + questions[j]["state"] + '?\n\n')
     #     for k in range(4):
     #         if(k == rand):
     #             option = questions[j]["capital"]
@@ -84,55 +69,115 @@ def testroute():
 
     #             while(rand2 == j):
     #                 rand2 = random.randint(0, 49)
-                
+
     #             option = questions[rand2]["capital"]
-    #         testFile.write(chr(65 + k) + '. ' + option + '\n\n') 
-        
+    #         testFile.write(chr(65 + k) + '. ' + option + '\n\n')
+
     #     testFile.write('\n\n\n')
-=======
-    testStr = ('Test' + '.doc')
-    answerStr = ('AnswerKey' + '.doc')
-    testFile = open(testPath / testStr, 'w+')
-    answerFile = open(answerPath / answerStr, 'w+')
-    answerFile.write('ANSWER KEY (FORM1)\n')
-    testFile.write(
-        'Name:\n\n\nDate:\n\n\nPeriod:\n\n\nState Capitals Quiz (Form1)\n\n\n')
-    random.shuffle(questions)
 
-    for j in range(50):
-        rand = random.randint(0, 3)
-        answerFile.write(str(j + 1) + '. ' + chr(65 + rand) + '\n')
-        testFile.write(str(j + 1) + '. What is the capital of ' +
-                       questions[j]["state"] + '?\n\n')
-        for k in range(4):
-            if(k == rand):
-                option = questions[j]["capital"]
-            else:
-                rand2 = random.randint(0, 49)
+    doc = Document()
 
-                while(rand2 == j):
-                    rand2 = random.randint(0, 49)
+    # generate header
+    section = doc.sections[0]
+    section.different_first_page_header_footer = True
+    header = section.first_page_header
+    header_para = header.paragraphs[0]
+    header_para.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.RIGHT
+    header_run = header_para.add_run()
+    header_font = header_run.font
+    header_font.size = Pt(12)
+    header_font.name = 'Helvetica'
 
-                option = questions[rand2]["capital"]
-            testFile.write(chr(65 + k) + '. ' + option + '\n\n')
+    header_run.text = "" \
+        "Name: \n" \
+        "Date: \n" \
+        "Section: "
 
-        testFile.write('\n\n\n')
->>>>>>> e788b8dec4a37872ff35ac101a7c9cc00c8c21cc
+    # generate title
+    title_para = doc.add_paragraph()
+    title_para.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    title_para.paragraph_format.space_before = Pt(12)
+    title_para.paragraph_format.space_after = Pt(12)
+    title_run = title_para.add_run()
+    title_font = title_run.font
+    title_font.name = 'Helvetica'
+    title_font.size = Pt(20)
+    title_font.bold = True
 
-        document.save(testPath / 'demo.docx')
+    title_run.text = "Exam 3: Thing"
+
+    # generate instructions
+    instructions_para = doc.add_paragraph()
+    instructions_para.paragraph_format.space_after = Pt(40)
+    instructions_run = instructions_para.add_run()
+    instructions_font = instructions_run.font
+    instructions_font.name = 'Helvetica'
+    instructions_font.size = Pt(12)
+
+    instructions_run.text = "" \
+        "Instructions: Lorem ipsum dolor sit amet, consectetur adipiscing elit, " \
+        "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim " \
+        "henderit in voluptate velit esse cillum dolore eu fugiat nulla paria"
+
+    # geneate section loop
+    TMP_SECTION_INDEX = 4
+    TMP_QUESTION_INDEX = 10
+    for i in range(TMP_SECTION_INDEX):
+        # generate section title
+        section_title_para = doc.add_paragraph()
+        section_title_para.paragraph_format.space_before = Pt(20)
+        section_title_para.paragraph_format.space_after = Pt(6)
+        section_title_run = section_title_para.add_run()
+        section_title_font = section_title_run.font
+        section_title_font.name = 'Helvetica'
+        section_title_font.size = Pt(12)
+        section_title_font.bold = True
+
+        section_title_run.text = "section " + f"{i + 1}"
+
+        # generate section instructions
+        section_instructions_para = doc.add_paragraph()
+        section_instructions_para.paragraph_format.space_before = Pt(6)
+        section_instructions_para.paragraph_format.space_after = Pt(10)
+        section_instructions_run = section_instructions_para.add_run()
+        section_instructions_font = section_instructions_run.font
+        section_instructions_font.name = 'Helvetica'
+        section_instructions_font.size = Pt(12)
+
+        section_instructions_run.text = "rem ipsum dolor sit amet, consectetur " \
+            "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna"
+
+        # generate questions
+        for j in range(TMP_QUESTION_INDEX):
+            question_para = doc.add_paragraph()
+            question_para.paragraph_format.space_before = Pt(12)
+            question_para.paragraph_format.space_after = Pt(12)
+            question_run = question_para.add_run()
+            question_font = question_run.font
+            question_font.name = 'Helvetica'
+            question_font.size = Pt(12)
+            question_run.text = f"Question {j + 1} \n" \
+                "_______________"
+
+    testPath = Path.home() / 'Tests'
+    answerPath = Path.home() / 'AnswerKeys'
+
+    if(not(os.path.isdir(testPath))):
+        os.mkdir(testPath)
+
+    if(not(os.path.isdir(answerPath))):
+        os.mkdir(answerPath)
+
+    os.chdir(testPath)
+    doc.save('demo.docx')
+
+    os.chdir(Path.home())
     return (
         send_file(
-<<<<<<< HEAD
-        testPath / 'demo.docx', 
-        mimetype = 'doc', 
-        attachment_filename = 'Test.doc', 
-        as_attachment = True
-=======
-            Path.home() / 'Tests' / 'Test1.doc',
-            mimetype='doc',
-            attachment_filename='Test.doc',
+            testPath / 'demo.docx',
+            mimetype='docx',
+            download_name='Test.docx',
             as_attachment=True
->>>>>>> e788b8dec4a37872ff35ac101a7c9cc00c8c21cc
         )
     )
 
