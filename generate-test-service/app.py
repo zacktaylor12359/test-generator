08641186e373a98ef8177d8cd3589d272dc38a35
zacktaylor12359 <46452540+zacktaylor12359@users.noreply.github.com>
2022-06-06 16:37:18 -0400

    heroku test 2

--- generate-test-service/app.py
+++ generate-test-service/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 def change(amount):
     # calculate the resultant change and store the result (res)
-    # heroku test
+    # heroku test 2
     res = []
     coins = [1, 5, 10, 25]  # value of pennies, nickels, dimes, quarters
     coin_lookup = {25: "quarters", 10: "dimes", 5: "nickels", 1: "pennies"}
